9d54b4482973a0d9393fe47c1f836765db1941e2
João Melo <joao.melo@m4u.com.br>
2020-08-26 01:52:55 -0300

    Inclusão de funcionalidades (Incluir contato/Excluir contato) e outras regras de negócio.

--- src/ui/MainForm.java
+++ src/ui/MainForm.java
@@ -1,9 +1,16 @@
 package ui;
 
+import business.ContactBusiness;
+import entity.ContactEntity;
+
 import javax.swing.*;
+import javax.swing.event.ListSelectionEvent;
+import javax.swing.event.ListSelectionListener;
+import javax.swing.table.DefaultTableModel;
 import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.util.List;
 
 public class MainForm extends JFrame {
     private JPanel panel1;
@@ -11,6 +18,11 @@ public class MainForm extends JFrame {
     private JButton buttonRemove;
     private JButton buttonNewContact;
     private JTable tableContacts;
+    private JLabel labelContactCount;
+
+    private ContactBusiness mContactBusiness;
+    private String mName = "";
+    private String mPhone = "";
 
     public MainForm() {
 
@@ -23,8 +35,31 @@ public class MainForm extends JFrame {
 
         setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
 
-        setListeners();
+        mContactBusiness = new ContactBusiness();
 
+        setListeners();
+        loadContacts();
+    }
+
+    private void loadContacts() {
+        List<ContactEntity> contactList = mContactBusiness.getList();
+
+        String[] columnNames = {"Nome", "Telefone"};
+        DefaultTableModel model = new DefaultTableModel(new Object[0][0], columnNames);
+
+        for(ContactEntity i : contactList) {
+            Object[] o = new Object[2];
+
+            o[0] = i.getName();
+            o[1] = i.getPhone();
+
+            model.addRow(o);
+        }
+
+        tableContacts.clearSelection();
+        tableContacts.setModel(model);
+
+        labelContactCount.setText(mContactBusiness.getContactCountDescription());
     }
 
     private void setListeners() {
@@ -36,10 +71,23 @@ public class MainForm extends JFrame {
             }
         });
 
+        tableContacts.getSelectionModel().addListSelectionListener(new ListSelectionListener() {
+            @Override
+            public void valueChanged(ListSelectionEvent e) {
+                if(e.getValueIsAdjusting()) {
+
+                    if(tableContacts.getSelectedRow() != -1){
+                        mName = tableContacts.getValueAt(tableContacts.getSelectedRow(), 0).toString();
+                        mPhone = tableContacts.getValueAt(tableContacts.getSelectedRow(), 1).toString();
+                    }
+                }
+            }
+        });
+
         buttonRemove.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent actionEvent) {
-
+                mContactBusiness.delete(mName, mPhone);
             }
         });
     }
